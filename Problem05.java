140fb75a7b273d74d2303d8ca7613201447354d0
Juan Garcia <38033917+jgarcia45@users.noreply.github.com>
2019-09-25 22:33:03 -0700

    Update Problem05.java

--- Problem05.java
+++ Problem05.java
@@ -5,3 +5,39 @@
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 */
+
+public class HelloWorld{
+
+     public static void main(String []args){
+        
+        int pool[] = new int[20];
+      
+        pool[0] = 2;
+        int count = 1;
+        
+        for (int i = 3; i <= 20; i++) {
+            int j = i;
+            System.out.println(j);
+            
+            for (int a = 0; pool[a] != 0; a++) { 
+                if (j % pool[a] == 0) {
+                    j = j / pool[a];                
+                }
+            }
+            if (j == 1) {
+                
+            } else {
+                pool[count] = j;
+                count++;
+            }
+        }
+        int answer = 1;
+        for (int a = 0; pool[a] != 0; a++) {
+            answer *= pool[a];
+            System.out.println(answer);
+         
+        }       
+            System.out.println(answer);    
+     }
+}
+
